9585d35e8ca8f5b3a5d515a4f93dda56d94d9ad5
plmlysiak <maja.lysiak@harman.com>
2018-07-31 16:40:26 +0200

    NullPointerException

---
+++ src/main/java/letsDoIT/Controller.java
@@ -0,0 +1,60 @@
+package letsDoIT;
+
+
+import components.Priority;
+import components.Status;
+import components.Task1;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+
+public class Controller implements Initializable {
+
+    private final Task1 currentTask = new Task1();
+
+    private final ObservableList<Task1> tasks = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<Task1> taskTable;
+
+    @FXML
+    private TableColumn<Task1, Priority> priorityColumn;
+
+    @FXML
+    private TableColumn<Task1, Status> statusColumn;
+
+    @FXML
+    private TableColumn<Task1, String> titleColumn;
+
+    @FXML
+    private TableColumn<Task1, String> descriptionColumn;
+
+    @FXML
+    private TextField taskDescription;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        taskDescription.setText("dupa");
+
+        taskTable.setItems(tasks);
+        titleColumn.setCellValueFactory(rowData -> rowData.getValue().titleProperty());
+        descriptionColumn.setCellValueFactory(rowData -> rowData.getValue().descriptionProperty());
+
+        tasks.addAll(
+                new Task1(1,"Pierwszy task", "Taki sobie pierwszy task"),
+                new Task1(2,"Drugi task", "Taki sobie inny task"),
+                new Task1(3,"Trzeci task", "Taki sobie kolejny task"));
+
+    }
+}
---
+++ src/main/java/letsDoIT/Gui.java
@@ -0,0 +1,56 @@
+package letsDoIT;
+
+import components.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+
+public class Gui extends Application {
+
+    public static void main(String[] args) {
+
+//        TaskBoardImpl backlog = new TaskBoardImpl();
+//        User user1 = new UserImpl("maja@lysiak.com.pl", "Maja", "12345");
+//        Task firstTask = new Task("Task1", Priority.LOW, Status.TODO);
+//
+//        user1.addTask(firstTask, backlog);
+//
+//        backlog.showContent();
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = new File("src/main/java/letsDoIT/Gui.fxml").toURI().toURL();
+        HBox box = FXMLLoader.load(url);
+
+        primaryStage.setTitle("LetsDoIT");
+        primaryStage.setScene(new Scene(box));
+        primaryStage.show();
+
+    }
+
+}
+
+
+// For later - connection with database
+
+//    Database database = new Database();
+////        database.connect();
+////
+////                Task task1 = new Task("Task1", "Pierwszy task");
+////
+////                int ile = 20;
+////                for(int i = 0;i < ile; i++){
+////        System.out.println(("task: " + task1.getTitle() + " "+ task1.getDescription() + " has been added with id: ") + database.addRecord(task1)  );
+////
+////        }
